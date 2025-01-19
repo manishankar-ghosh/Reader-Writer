2314a16a8be22a278e698242c501e30790a18325
manishankar-ghosh <manishankar.ghosh.india@outlook.com>
2025-01-19 22:12:00 +0530

    Initial commit

---
+++ ReaderWriter/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ReaderWriter;
+
+Worker worker = new Worker();
+await worker.Start();
+
+Console.WriteLine("Done!");
---
+++ ReaderWriter/Worker.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReaderWriter
+{
+    internal class Worker
+    {
+        private AutoResetEvent _signal = new (false);
+        private ConcurrentQueue<int> _data = new();
+        private bool readInProgress;
+        private async Task ReadAsync()
+        {
+            readInProgress = true;
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Reading data...");
+                //_signal.Reset();
+                await Task.Delay(500);
+                _data.Enqueue(i);
+                _signal.Set();
+            }
+            _data.Enqueue(-1);
+            _signal.Set();
+            readInProgress = false;
+            Console.WriteLine("End of Reader Thread");
+        }
+
+        private async Task WritAsync()
+        {
+            while (true)
+            {
+                if (_data.IsEmpty || readInProgress)
+                {
+                    Console.WriteLine("Waiting for data...");
+                    _signal.WaitOne();
+                }
+
+                if(_data.TryDequeue(out int data))
+                {
+                    if (data == -1)
+                    {
+                        break;
+                    }
+
+                    Console.Write($"\r\nData received {data}. Writing into db...");
+                    await Task.Delay(3000);
+                    Console.WriteLine($" Write complete!");
+                }
+            }
+
+            Console.WriteLine("End of Writer Thread");
+        }
+
+        public async Task Start() 
+        { 
+            Task t1 = ReadAsync();
+            Task t2 = WritAsync();
+
+            await Task.WhenAll(t1, t2);
+        }
+    }
+}
