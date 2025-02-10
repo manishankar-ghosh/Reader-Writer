28bf3db46f2d07478a93abfa0a955b9f1ec9a557
Manishankar Ghosh <manishankar.ghosh.india@outlook.com>
2025-02-10 18:17:12 +0530

    Implemented reader threshold

--- ReaderWriter/Worker.cs
+++ ReaderWriter/Worker.cs
@@ -9,23 +9,35 @@ namespace ReaderWriter
 {
     internal class Worker
     {
-        private AutoResetEvent _signal = new (false);
+        private AutoResetEvent _signalReader = new(false);
+        private AutoResetEvent _signalWriter = new(false);
+        private int _readerThresholdCount = 3;
         private ConcurrentQueue<int> _data = new();
         private bool readInProgress;
         private async Task ReadAsync()
         {
             readInProgress = true;
-            for (int i = 0; i < 5; i++) // Simulate few reads
+            for (int i = 0; i < 10; i++) // Simulate few reads
             {
-                Console.WriteLine("Reading data...");
+                if(_data.Count > _readerThresholdCount)
+                {
+                    var timeNow = DateTime.Now;
+                    Console.WriteLine("Reader Threshold reached halting for some time.");
+                    _signalWriter.WaitOne();
+                    var gap = (DateTime.Now - timeNow).TotalMilliseconds;
+                    Console.WriteLine($"Resuming read after {gap} ms");
+                }
+                Console.Write("\r\nReader --> Reading data...");
                 //_signal.Reset();
-                await Task.Delay(500);
+                await Task.Delay(1000);
                 _data.Enqueue(i);
-                _signal.Set();
+                Console.WriteLine($"Put data value '{i}' into queue.");
+                _signalReader.Set();
             }
-            _data.Enqueue(-1);
-            _signal.Set();
+            //_data.Enqueue(-1);
             readInProgress = false;
+
+            _signalReader.Set();
             Console.WriteLine("End of Reader Thread");
         }
 
@@ -33,30 +45,35 @@ namespace ReaderWriter
         {
             while (true)
             {
-                if (_data.IsEmpty || readInProgress)
+                if (_data.IsEmpty && readInProgress)
                 {
-                    Console.WriteLine("Waiting for data...");
-                    _signal.WaitOne();
+                    Console.WriteLine("Writer --> Waiting for data...");
+                    _signalReader.WaitOne();
                 }
-
-                if(_data.TryDequeue(out int data))
+                else if (_data.TryDequeue(out int data))
                 {
-                    if (data == -1)
-                    {
-                        break;
-                    }
+                    _signalWriter.Set();
 
-                    Console.Write($"\r\nData received {data}. Writing into db...");
-                    await Task.Delay(3000);
+                    //if (data == -1)
+                    //{
+                    //    break;
+                    //}
+
+                    Console.WriteLine($"\r\nDequed data value '{data}'. Writing into db...");
+                    await Task.Delay(2000);
                     Console.WriteLine($" Write complete!");
+                }
+                else
+                {
+                    break;
                 }
             }
 
             Console.WriteLine("End of Writer Thread");
         }
 
-        public async Task Start() 
-        { 
+        public async Task Start()
+        {
             Task t1 = ReadAsync();
             Task t2 = WritAsync();
 
